ead2483590b1151635b166e20b0dfb27106ab537
alifrontend499 <aliofficial499@gmail.com>
2017-12-27 00:43:10 +0530

    canvas first example completed

--- js/canvas.js
+++ js/canvas.js
@@ -5,12 +5,12 @@ window.onload = function () {
     document.querySelector('body').style.cssText = bodtyStyle
     var elem = document.getElementById('heading')
     elem.style.cssText = 'padding:10px 15px;text-transform:uppercase;text-align:center;'
-    elem.innerHTML = '<h1>Html Canvas</h1>'
+    // elem.innerHTML = '<h1>Html Canvas</h1>'
   })()
   // canvas
   var cv = document.querySelector('#myCanvas')
-  cv.width = 700
-  cv.height = 400
+  cv.width = window.innerWidth - 50
+  cv.height = window.innerHeight - 50
   var c = cv.getContext('2d')
   // console.dir(c)
   // Line
@@ -52,6 +52,23 @@ window.onload = function () {
   //   c.strokeStyle = 'rgb(' + color + ', 12, 23)'
   //   c.stroke()
   // }
+  // perform action on mouse move
+  var mousePosition = {
+    x: undefined,
+    y: undefined
+  }
+  var maxRadius = 80
+  var colorArray = ['#04BBBF', '#D2D945', '#FCC23F', '#FF694F', 'purple']
+  // console.log(randomColor)
+  cv.addEventListener('mousemove', function (e) {
+    mousePosition.x = e.x
+    mousePosition.y = e.y
+  })
+  window.addEventListener('resize', function () {
+    cv.width = window.innerWidth - 50
+    cv.height = window.innerHeight - 50
+    init()
+  })
   // Moving Animation
   function Circle (x, y, dx, dy, radius) {
     this.x = x
@@ -59,12 +76,16 @@ window.onload = function () {
     this.dx = dx
     this.dy = dy
     this.radius = radius
+    this.minradius = radius
+    this.color = colorArray[Math.floor(Math.random() * colorArray.length)]
     this.draw = function () {
       c.beginPath()
       c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true)
       c.strokeStyle = 'blue'
-      c.fill()
       c.stroke()
+      // c.fillStyle = colorArray[Math.floor(Math.random() * colorArray.length)]
+      c.fillStyle = this.color
+      c.fill()
     }
     this.update = function () {
       if (this.x + this.radius > cv.width || this.x - this.radius < 0) {
@@ -75,18 +96,29 @@ window.onload = function () {
       }
       this.x += this.dx
       this.y += this.dy
+      if (mousePosition.x - this.x < 50 && mousePosition.x - this.x > -50 && mousePosition.y - this.y < 50 && mousePosition.y - this.y > -50) {
+        if (this.radius < maxRadius) {
+          this.radius += 1
+        }
+      } else if (this.radius > this.minradius) {
+        this.radius -= 1
+      }
       this.draw()
     }
   }
   var circleArray = []
-  for (var i = 0; i < 100; i++) {
-    var cirradius = 25
-    var x = Math.random() * (cv.width - cirradius * 2) + cirradius
-    var y = Math.random() * (cv.height - cirradius * 2) + cirradius
-    var dx = (Math.random() - 0.5)
-    var dy = (Math.random() - 0.5)
-    circleArray.push(new Circle(x, y, dx, dy, cirradius))
+  function init () {
+    circleArray = []
+    for (var i = 0; i < 1000; i++) {
+      var cirradius = Math.random() * 3 + 1
+      var x = Math.random() * (cv.width - cirradius * 2) + cirradius
+      var y = Math.random() * (cv.height - cirradius * 2) + cirradius
+      var dx = (Math.random() - 0.5)
+      var dy = (Math.random() - 0.5)
+      circleArray.push(new Circle(x, y, dx, dy, cirradius))
+    }
   }
+  init()
   var animations = function () {
     window.requestAnimationFrame(animations)
     c.clearRect(0, 0, cv.width, cv.height)
@@ -95,11 +127,4 @@ window.onload = function () {
     }
   }
   animations()
-  var xx
-  function inc () {
-    console.log(xx)
-  }
-  for (xx = 0; xx < 10; xx++) {
-    inc()
-  }
 }
